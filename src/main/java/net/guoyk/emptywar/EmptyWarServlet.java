2e7b6adddafabee84d35451724e20940fe3508ac
Guo Y.K <hi@guoyk.net>
2021-04-29 14:33:40 +0800

    complete

---
+++ src/main/java/net/guoyk/emptywar/EmptyWarServlet.java
@@ -0,0 +1,20 @@
+package net.guoyk.emptywar;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EmptyWarServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/plain");
+        resp.setStatus(200);
+        resp.getWriter().write("Hello World from EmptyWar");
+    }
+
+}
